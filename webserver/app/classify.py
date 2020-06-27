0f1293fc8d1897b7277f63844303eae860e3c7dc
quanmou <quanzhch@163.com>
2020-06-28 00:16:17 +0800

    add web server to display model effect

---
+++ webserver/app/classify.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+import json
+import os
+import sys
+import tensorflow as tf
+
+DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+if DIR not in sys.path:
+    sys.path.append(DIR)
+
+
+from src.bert.practice_clf import BertClassifier
+
+
+app = Flask(__name__)
+
+bert_ckpt1 = tf.train.latest_checkpoint(f"{DIR}/model/practice_clf/2020-06-27_21")
+bert_clf = BertClassifier(init_checkpoint=bert_ckpt1)
+
+
+@app.route('/jd', methods=['GET', 'POST'])
+def practice():
+    recv_data = request.get_data()
+    res = {}
+    if recv_data:
+        print(recv_data.decode())
+        json_re = json.loads(recv_data)
+        segment = json_re['segment']
+        res['bert_topK_labels'] = bert_clf.predict(segment, topK=10)
+
+    return json.dumps(res)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('jd_classify.html')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        p = int(sys.argv[1])
+    else:
+        p = 9005
+    app.run(host='0.0.0.0',  # 任何ip都可以访问
+            port=p,       # 端口
+            debug=False
+            )
