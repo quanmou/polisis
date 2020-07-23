00d6c8638369498e1c33230e2d60c4c93c77862e
quanmou <quanzhch@163.com>
2020-07-24 01:38:08 +0800

    添加每个分类的属性映射表

---
+++ data/category_attributes.py
@@ -0,0 +1,18 @@
+
+
+category_attributes = {
+    "First Party Collection/Use": ["Choice Scope", "Choice Type", "User Type", "Personal Information Type",
+                                   "Purpose", "Identifiability", "Does/Does Not", "Collection Mode",
+                                   "Action First-Party"],
+    "Third Party Sharing/Collection": ["Choice Scope", "Choice Type", "User Type", "Personal Information Type",
+                                       "Purpose", "Identifiability", "Does/Does Not", "Action Third Party",
+                                       "Third Party Entity"],
+    "User Choice/Control": ["Choice Scope", "Choice Type", "User Type", "Personal Information Type", "Purpose"],
+    "User Access, Edit and Deletion": ["User Type", "Access Type", "Access Scope"],
+    "Data Retention": ["Personal Information Type", "Retention Period", "Retention Purpose"],
+    "Data Security": ["Security Measure"],
+    "Policy Change": ["Change Type", "User Choice", "Notification Type"],
+    "Do Not Track": ["Do Not Track policy"],
+    "International and Specific Audiences": ["Audience Type"],
+    "Other": ["Other Type"]
+}
